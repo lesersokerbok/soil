780a2e00b2ddc14e3a87506ccdbfefe2854b6cef
Tom Erik Støwer <testower@gmail.com>
2014-12-18 14:01:57 +0100

    Keep (other) url query parameters in nice search redirect

--- modules/nice-search.php
+++ modules/nice-search.php
@@ -3,7 +3,7 @@
  * Redirects search results from /?s=query to /search/query/, converts %20 to +
  *
  * @link http://txfx.net/wordpress-plugins/nice-search/
- * 
+ *
  * You can enable/disable this feature in functions.php (or lib/config.php if you're using Roots):
  * add_theme_support('soil-nice-search');
  */
@@ -15,7 +15,24 @@ function soil_nice_search_redirect() {
 
   $search_base = $wp_rewrite->search_base;
   if (is_search() && !is_admin() && strpos($_SERVER['REQUEST_URI'], "/{$search_base}/") === false) {
-    wp_redirect(home_url("/{$search_base}/" . urlencode(get_query_var('s'))));
+
+    $search_string = urlencode(get_query_var('s'));
+
+    global $wp_query;
+    $query_vars = $wp_query->query;
+
+    if (count($query_vars) > 1) {
+      $search_string .= '?';
+      foreach ($query_vars as $key => $value) {
+        if ($key != 's') {
+          $search_string .= $key . '=' . $value . '&';
+        }
+      }
+      $search_string = rtrim($search_string, "&");
+    }
+
+    wp_redirect(home_url("/{$search_base}/" . $search_string));
+    
     exit();
   }
 }
